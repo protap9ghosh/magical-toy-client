92c3567e0ea953e31b15589f9b17bc04710630c8
Protap Ghosh <protapg518@gmail.com>
2023-05-19 22:59:05 +0600

    all toys page style

--- src/pages/toys/AllToys/AllToys.jsx
+++ src/pages/toys/AllToys/AllToys.jsx
@@ -18,17 +18,25 @@ const AllToys = () => {
 
    return (
       <div className='bg-gray-50 py-10'>
-         <h2 className='text-5xl text-stone-700 text-center font-bold'>All Toys</h2>
-         <p className='w-1/2 mx-auto my-8 text-slate-500 text-[18px] text-center font-semibold'>Unleash your creativity and bond with furry friends through Toy Time Fun a collection of animal toys designed to inspire laughter and endless fun</p>
+         <h2 className='text-4xl text-stone-700 text-center font-bold mb-6'>All Toys Items</h2>
+
+         <div className="form-control mb-8">
+            <div className="input-group w-2/6 mx-auto">
+               <input type="text" placeholder="Search…" className="input input-bordered w-full" />
+               <button className="btn btn-square">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+               </button>
+            </div>
+         </div>
+
          <div>
             <div className="container mx-auto overflow-x-auto w-full">
-               <table className="table w-full">
+               <table className="table table-zebra w-full">
                   {/* head */}
                   <thead>
                      <tr>
-                        <th>Image</th>
+                        <th>Seller</th>
                         <th>toy Name</th>
-                        <th>Seller Name</th>
                         <th>Sub Category</th>
                         <th>Available Quantity</th>
                         <th>Price</th>
--- src/pages/toys/Toy/Toy.jsx
+++ src/pages/toys/Toy/Toy.jsx
@@ -6,24 +6,15 @@ const toy = ({ toy }) => {
 
    return (
       <tr>
-         <td>
-            <div className="flex items-center space-x-3">
-               <div className="avatar">
-                  <div className="mask mask-hexagon w-28 h-28">
-                     {photo && <img src={photo} alt="Photo"/>}
-                  </div>
-               </div>
-            </div>
-         </td>
-         <td>
-            <div className="font-bold">{toyName}</div>
-         </td>
          <td>{sellerName}</td>
+         <td>
+            <div className="font-semibold">{toyName}</div>
+         </td>
          <td>{category}</td>
          <td>{quantity}</td>
          <td>${price}</td>
          <th>
-            <Link to={`/toy-details/${_id}`} className="btn btn-outline btn-info btn-sm">Toy details</Link>
+            <Link to={`/toy-details/${_id}`} className="btn btn-outline btn-sm rounded-sm capitalize">Toy details</Link>
          </th>
       </tr>
    );
