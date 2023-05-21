8067e6601a962ddb6d92a35dac6f2aacd58d5ce6
Protap Ghosh <protapg518@gmail.com>
2023-05-21 23:03:29 +0600

    add design component

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import Blog from '../../Blog/Blog';
 import ShopCategory from '../../ShopCategory/ShopCategory';
 import useTitle from '../../../hooks/useTitle';
+import Banner from '../Banner/Banner';
+import PhotoGallery from '../../PhotoGallery.jsx/PhotoGallery';
+import Products from '../../Products/Products';
+import Services from '../../Services/Services';
 
 const Home = () => {
    useTitle('Home');
 
    return (
-      <div className='container mx-auto'>
+      <div className=''>
+         <Banner></Banner>
+         <PhotoGallery></PhotoGallery>
          <ShopCategory></ShopCategory>
+         <Services></Services>
+         <Products></Products>
       </div>
    );
 };
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
 
 const NotFound = () => {
    return (
-      <div>
-         
+      <div className="hero min-h-screen bg-base-200">
+         <div className="hero-content text-center">
+            <div>
+               <div className='mb-8'>
+                  <img src="/src/assets/notFound.jpg" alt="" />
+               </div>
+               <h1 className="text-5xl font-bold">Oops! <br />Page not found</h1>
+               <p className="py-6">The page you are looking for could not be found.</p>
+               <Link to="/" className="btn btn-outline btn-primary"> <FaArrowLeft className='mr-2'/> Back to Home</Link>
+            </div>
+         </div>
       </div>
    );
 };
